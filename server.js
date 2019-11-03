908526bebff38d83a25109d41ecc60f35acc5516
Podobrieieva <juliapodobr@gmail.com>
2019-11-03 18:19:45 +0200

    create project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 5050;
+
+app.get("/api", (req, resp) => {
+  resp.set("Access-Control-Allow-Origin", "http://localhost:8080");
+  resp.json({ x: 5 });
+});
+
+app.use(express.static("dist"));
+
+app.listen(port, () => {
+  console.log("object");
+});
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import HelloWorld from "./components/HelloWorld.vue";
+
+const API_URL = "http://localhost:5050/api";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  },
+
+  async mounted() {
+    // eslint-disable-next-line no-console
+    await axios.get(API_URL).then(response => console.log(response));
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
