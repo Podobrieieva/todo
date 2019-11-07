a5d75cd0af80778e7428ca1ffee236630bf3803b
Yuliia Podobrieieva <yuliia.podobrieieva@dev-pro.net>
2019-11-07 18:54:24 +0200

    add vue material

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TodoInput />
+    <TodoList />
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoList from "./components/TodoList";
+import TodoInput from "./components/TodoInput";
 
 const API_URL = process.env.VUE_APP_API_URL;
 
 export default {
-  name: "app",
+  name: "App",
+  
   components: {
-    HelloWorld
+    TodoList,
+    TodoInput
   },
 
   async created() {
-    console.log(API_URL);
-    console.log(process.env)
-    // eslint-disable-next-line no-console
     await axios.get(API_URL).then(response => console.log(response));
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
+import { MdButton, MdField  } from 'vue-material/dist/components'
 import App from './App.vue'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdField)
+
 
 Vue.config.productionTip = false
 
